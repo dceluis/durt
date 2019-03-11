f15e630b48c82aba22193e8b1f6414a04b043411
Luis Felipe Sanchez <sanchezhorna@outlook.com>
2019-03-11 14:40:05 -0500

    Remove unnecessary config methods

--- lib/durt/status.rb
+++ lib/durt/status.rb
@@ -5,9 +5,5 @@ require_relative 'application_record'
 module Durt
   class Status < ApplicationRecord
     scope :active, -> { where(active: true) }
-
-    def self.config?
-      !active.empty?
-    end
   end
 end
