913f85adbdadfb522bd3f989f498a74ff872b531
Luis Felipe Sanchez <sanchezhorna@outlook.com>
2019-03-11 13:06:13 -0500

    Reference production database correctly

--- lib/durt/application_record.rb
+++ lib/durt/application_record.rb
@@ -3,9 +3,10 @@
 require 'active_record'
 
 db_config_file_path = File.expand_path('../../db/config.yml', __dir__)
-db_config = YAML.load(File.read(db_config_file_path))
+db_config = YAML.load(File.read(db_config_file_path))['production']
+db_config[:database] = File.expand_path('../../' + db_config['database'], __dir__)
 
-ActiveRecord::Base.establish_connection(db_config['development'])
+ActiveRecord::Base.establish_connection(db_config)
 
 module Durt
   class ApplicationRecord < ::ActiveRecord::Base
