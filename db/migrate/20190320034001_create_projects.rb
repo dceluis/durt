c81a28ce22949aee66cd02ea116b1c73f52a5ad3
Luis Felipe Sanchez <sanchezhorna@outlook.com>
2019-03-20 12:06:57 -0500

    Add active column to projects

--- db/migrate/20190320034001_create_projects.rb
+++ db/migrate/20190320034001_create_projects.rb
@@ -2,6 +2,7 @@ class CreateProjects < ActiveRecord::Migration[5.2]
   def change
     create_table :projects do |t|
       t.string :name, null: false
+      t.boolean :active, null: false, default: false
     end
   end
 end
