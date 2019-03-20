b13cb6dbb8d850f9c28ebd1aa9fffce5453eae50
Luis Felipe Sanchez <sanchezhorna@outlook.com>
2019-03-20 12:13:40 -0500

    Remove unused methods

--- lib/durt/project.rb
+++ lib/durt/project.rb
@@ -25,14 +25,6 @@ module Durt
     def active_issue
       issues.find_by!(active: true)
     end
-
-    # def bug_trackers
-    #   plugins.find_all { |plugin| !plugin.bug_tracker.nil? }
-    # end
-    #
-    # def time_trackers
-    #   plugins.find_all { |plugin| !plugin.time_tracker.nil? }
-    # end
   end
 end
 
