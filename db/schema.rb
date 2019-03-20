747076674dbf3edf306caf75f5626ff20a847a9b
Luis Felipe Sanchez <sanchezhorna@outlook.com>
2019-03-19 22:47:39 -0500

    Add Projects migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_03_11_072306) do
+ActiveRecord::Schema.define(version: 2019_03_20_034001) do
 
   create_table "issues", force: :cascade do |t|
     t.string "key", null: false
@@ -20,6 +20,10 @@ ActiveRecord::Schema.define(version: 2019_03_11_072306) do
     t.boolean "active", default: false, null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "projects", force: :cascade do |t|
+    t.string "name", null: false
   end
 
   create_table "sessions", force: :cascade do |t|
