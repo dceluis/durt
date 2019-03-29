afc99731f2f7b0ec9787b42daca118c2d661ea50
Luis Felipe Sanchez <sanchezhorna@outlook.com>
2019-03-29 00:52:41 -0500

    Add all other commands

--- lib/durt/command.rb
+++ lib/durt/command.rb
@@ -10,6 +10,36 @@ module Durt
       end
     end
 
+    class SelectProject < ::Durt::Service
+      def call
+        Durt::Project.select!
+        Durt::Project
+          .current_project
+          .time_tracker_plugins
+          .each(&:switch_project)
+      end
+    end
+
+    class Filter < ::Durt::Service
+      def call
+        Durt::Project.current_project.plugins.each(&:filter)
+      end
+    end
+
+    class Memo < ::Durt::Service
+      def call
+        bt_plugin = Durt::Project.select_source(current_project)
+
+        bt_plugin.before_choose
+        issue = bt_plugin.choose
+
+        tt_plugins = current_project.time_tracker_plugins
+
+        tt_plugins.each { |plugin| plugin.before_enter(issue) }
+        tt_plugins.each { |plugin| plugin.enter(issue) }
+      end
+    end
+
     class Start < ::Durt::Service
       def call
         Durt::Project.current_project.plugins.each(&:start)
@@ -22,9 +52,15 @@ module Durt
       end
     end
 
-    class Stats
+    class Stats < ::Durt::Service
       def call
         Durt::Project.current_project.active_issue.puts_stats
+      end
+    end
+
+    class StatsAll < ::Durt::Service
+      def call
+        Durt::Project.current_project.issues.each(&:puts_stats)
       end
     end
   end
