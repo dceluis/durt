2761d63f6767afff09c070cb071e858c4387b4e4
Luis Felipe Sanchez <sanchezhorna@outlook.com>
2020-06-01 15:50:00 -0500

    Update Octokit config

--- lib/durt/github_plugin.rb
+++ lib/durt/github_plugin.rb
@@ -5,7 +5,7 @@ require_relative 'plugin'
 module Durt
   class GithubPlugin < Plugin
     def self.demo_config
-      { login: 'username', password: 'password!', repo: 'rails/rails' }
+      { access_token: '<your 40 char token>', repo: 'rails/rails' }
     end
 
     def bug_tracker_class
